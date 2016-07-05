fc38e0776684b453c7cc12253914f4ce78c0a22e
Daniel Paulino <danielpaulino030@gmail.com>
2016-07-05 11:41:39 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['angular'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('angular'));
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory(root.angular);
+    }
+}(this, function (angular) {
+
+    return angular
+        .module('dragtable', [])
+        .directive('dragtable', function () {
+            return {
+                restrict: 'A',
+                link: function ($scope, element) {
+                    element.dragtable({
+                        clickDelay: 100,
+                        persistState: function () {
+                            const ths = element.find('thead tr th');
+                            const columns = ths.map((element) => {
+                                element.getAttribute('property-name');
+                            });
+
+                            $scope.$emit('dragtable:end', columns);
+                        }
+                    });
+                }
+            };
+        });
+
+}));
